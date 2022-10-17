3858bf448f5fb5cffb54d83f59a6cbe90333fc7a
Exequiel Herrera De Rosa <exequiel.herrera@globant.com>
2022-10-17 11:00:33 -0300

    Implementado el control de victoria sobre la matriz semantica de acarreo de estado

--- app/source/PlayerExe.cpp
+++ app/source/PlayerExe.cpp
@@ -4,6 +4,7 @@
 #include "Tile.h"
 #include <iostream>
 #include <random>
+#include <vector>
 
 namespace ttt
 {
@@ -13,11 +14,12 @@ namespace ttt
 		return { std::rand() % board.width(), std::rand() % board.height() };
 	}
 
-	static void printBoardState(Board const& board,int ** boardManagement)
+	static void printBoardState(Board const& board,int ** boardManagement) //Imprime el estado del tablero semántico.
 	{
-		for (int i = 0 ; i < board.height() ; i++){
-			for (int j = 0 ; j < board.width() ; j++){
-				std::cout<<boardManagement[j][i]<<" | ";
+		std::cout<<"BUILD"<<std::endl;
+		for (int i = 0 ; i < board.width() ; i++){
+			for (int j = 0 ; j < board.height() ; j++){
+				std::cout<<"{ "<<i<<" , "<<j<<" }: "<<boardManagement[i][j]<<" | ";
 			}
 			std::cout<<std::endl;
 		}
@@ -26,23 +28,136 @@ namespace ttt
 		std::cout<<std::endl;
 	}
 
-
-	static Coordinates play(Board const& board, int ** boardManagement, std::string name) 
+	static void executeMovement(int ** boardManagement, Coordinates c, bool actualPlayer)
 	{
-		//std::cout<<"Nueva Build"<<std::endl;
-		
-		
-		if (board.valid())                                                                   
+		if (actualPlayer){
+			boardManagement[c.x][c.y] = 1;}
+		else{
+			boardManagement[c.x][c.y] = 2;}
+
+	}
+
+
+	static std::vector<Coordinates> getMovs(Board const& board,int ** boardManagement) //Obtenemos todos los posibles movimientos a realizar del tablero.
+	{
+		std::vector<Coordinates> output;
+		output.clear();
+		for (int i = 0 ; i < board.width() ; i++){
+			for (int j = 0 ; j < board.height() ; j++){
+				if (boardManagement[i][j]==0){
+					output.push_back(Coordinates{i,j});
+				}
+			}
+		}
+		return output;
+	}
+
+
+
+	static bool gameOver(Board const& board, int ** boardManagement)
+	{
+
+		bool fullBoard = true; //Supongo que el tablero esta completo.
+		int width = board.width();
+		int height = board.height();
+		for (int i = 0 ; i < width ; i++ )
 		{
-			printBoardState(board,boardManagement);
+			for (int j = 0 ; j < height ; j++)
+			{
+				if ((boardManagement[i][j])==0){
+					fullBoard=false;
+				}
+				if (i+2<width){
+					if (boardManagement[i][j] == boardManagement[i+1][j] &&  boardManagement[i+1][j] == boardManagement[i+2][j] && boardManagement[i][j]!=0){
+						return true;
+					}
+				}
+				if (j+2< height){
+					if (boardManagement[i][j] == boardManagement[i][j+1] && boardManagement[i][j+1] ==  boardManagement[i][j+2] && boardManagement[i][j]!=0){
+						return true;
+					}
+				}
+				if (j+2<height && i+2 < width){
+					if ((boardManagement[i][j] == boardManagement[i+1][j+1] && boardManagement[i+1][j+1] == boardManagement[i+2][j+2] && boardManagement[i][j]!=0)   ||
+						(boardManagement[i][j+2] == boardManagement[i+1][j+1] && boardManagement[i+1][j+1] == boardManagement[i+2][j] && boardManagement[i][j+2]!=0)) {
+							return true;
+						}
+				}
+			}
+		
+		}
+		return fullBoard; //Si todas al menos una ficha sin colocar, esto dará falso. En caso que todas hayan estado ocupadas, se llego a un empate.
+	}
+
+
+	static int minimax(int depth, int alpha, int beta, bool isCurrentPlayer, Board const& board, int ** boardManagement)
+	{
+		std::vector<Coordinates> movs = getMovs(board,boardManagement);
+		bool pruning =false;
+		//if ((depth = 0)) 
+		
+		
+		
+
+	}
+
+
+	static Coordinates play(Board const& board, int ** boardManagement, std::string name)  //Obtenemos la coordenada final de movimiento.
+	{
+
+
+
+		if (board.valid()){
+			//std::cout<<"Nueva Build"<<std::endl;
+			std::vector<Coordinates> movs = getMovs(board,boardManagement);
+			/*for (Coordinates c : movs){ std::cout<<"Lista de movimientos, posible candidato: "<<c.x<<" , "<<c.y<<" ."<<std::endl;}*/
+			//printBoardState(board,boardManagement);
+
 			Coordinates coords = randomCoordinate(board);
+			bool actualPlayer = true; //Acarrea el contexto de cual jugador esta por poner ficha. Si es verdadero, es aquel jugador que llamo en un primer momento al metodo.
+			//De lo contrario, es su rival.
+
+			int alpha = -std::numeric_limits<int>::max(); //-Infinite
+			int beta = std::numeric_limits<int>::max(); //Infinite.
+			int best_alpha = alpha;
+			/*for (Coordinates candidate : movs){ 
+				executeMovement(boardManagement,candidate,actualPlayer); //Consumimos el movimiento.
+				//alpha=max(alpha,minimax(depth-1, alpha,beta,!actualPlayer, board,boardManagement));
+				if (alpha > best_alpha){
+					best_alpha = alpha;
+					coords = candidate;
+				}
+				boardManagement[candidate.x][candidate.y] = 0; //Quitamos el movimiento.
+			}*/
+
+
+			
+
+
+
+
+
+
+
+
 			while ((board.tile(coords) == nullptr) || board.tile(coords)->owner().has_value())
 			{
 				coords = randomCoordinate(board);
 			}
+
+			executeMovement(boardManagement,coords,actualPlayer);
+			printBoardState(board,boardManagement);
+			gameOver(board,boardManagement);
+
+
+
+
+
+
+
+			movs.clear();
 			return coords;
 		}
-
 		return { 0, 0 };
 	}
 
@@ -56,6 +171,8 @@ namespace ttt
 	//0 --> No lo tiene nadie.
 	//2 --> Jugador contrincante.
 	
+
+
 
 
 	static int** initializeAlphaBeta(Board const& board, std::string const& name)
@@ -103,6 +220,74 @@ namespace ttt
 		int  ** boardManagement = ttt::initializeAlphaBeta(board,name()); //En este momento hacemos la captura de la matriz actual.
 		return ttt::play(board,boardManagement,name());
 	}
+
+
+
+
+/*
+	static bool gameOver(Board const& board, int ** boardManagement)
+	{
+
+		bool fullBoard = true; //Supongo que el tablero esta completo.
+		int width = board.width();
+		int height = board.height();
+
+	
+		for (int i = 0 ; i < width ; i++ )
+		{
+			for (int j = 0 ; j < height ; j++)
+			{
+				if ((boardManagement[i][j])==0){
+					fullBoard=false;
+				}
+				if (i+2<width){
+					if (boardManagement[i][j] == boardManagement[i+1][j] &&  boardManagement[i+1][j] == boardManagement[i+2][j] && boardManagement[i][j]!=0){
+						std::cout<<"GAME OVER 1, con { "<<i<<","<<j<<"} valor: "<<boardManagement[i][j]<<" , { "<<i+1<<","<<j<<"} valor: "<<boardManagement[i+1][j]<<" , { "<<i+2<<","<<j<<"} valor: "<<boardManagement[i+2][j]<< std::endl;
+						return true;
+					}
+				}
+				if (j+2< height){
+					if (boardManagement[i][j] == boardManagement[i][j+1] && boardManagement[i][j+1] ==  boardManagement[i][j+2] && boardManagement[i][j]!=0){
+						std::cout<<"GAME OVER 2, con { "<<i<<","<<j<<"} valor: "<<boardManagement[i][j]<<" , { "<<i<<","<<j+1<<"} valor: "<<boardManagement[i][j+1]<<" , { "<<i<<","<<j+2<<"} valor: "<<boardManagement[i][j+2]<< std::endl;
+						return true;
+					}
+				}
+				if (j+2<height && i+2 < width){
+					if ((boardManagement[i][j] == boardManagement[i+1][j+1] && boardManagement[i+1][j+1] == boardManagement[i+2][j+2] && boardManagement[i][j]!=0)   ||
+						(boardManagement[i][j+2] == boardManagement[i+1][j+1] && boardManagement[i+1][j+1] == boardManagement[i+2][j] && boardManagement[i][j+2]!=0)) {
+						std::cout<<"GAME OVER 3, con { "<<i<<","<<j<<"} valor: "<<boardManagement[i][j]<<" , { "<<i+1<<","<<j+1<<"} valor: "<<boardManagement[i+1][j+1]<<" , { "<<i+2<<","<<j+2<<"} valor: "<<boardManagement[i+2][j+2]<< std::endl;
+						std::cout<<"GAME OVER 4, con { "<<i<<","<<j+2<<"} valor: "<<boardManagement[i][j+2]<<" , { "<<i+1<<","<<j+1<<"} valor: "<<boardManagement[i+1][j+1]<<" , { "<<i+2<<","<<j<<"} valor: "<<boardManagement[i+2][j]<< std::endl;
+	
+							return true;
+						}
+				}
+			}
+		
+		}
+		if (fullBoard){
+			std::cout<<"HAY EMPATE"<<std::endl;
+		}
+		return fullBoard; //Si todas al menos una ficha sin colocar, esto dará falso. En caso que todas hayan estado ocupadas, se llego a un empate.
+	}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- app/source/PlayerExe.h
+++ app/source/PlayerExe.h
@@ -11,10 +11,12 @@ public:
 	PlayerExe(std::string const& name);                          //Player Name.
 	std::string const& name() const override;
 	Coordinates play(Board const& board) const override; 
-	void initializeAlphaBeta(Board const& board, std::string const& name)   ;   
+	void initializeAlphaBeta(Board const& board, std::string const& name); 
 
 private:
 	std::string mName;
+
+	
 
 
 
