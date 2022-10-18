3be0c18adaece421c753306f3d9e6301f52b0280
francisco.luque <francisco.luque@globant.com>
2022-10-18 11:10:46 -0300

    Revert "Remove unnecessary change from Board.h"
    This reverts commit 998ca57f130473ce989e1b2084c6637180dc84f7.

---
+++ logic/include/Board.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+namespace ttt
+{
+
+class Tile;
+class IPlayer;
+struct Coordinates;
+
+class Board
+{
+public:
+	Board(int width = 3, int height = 3, int winCondition = 3);
+	Board(Board const& other);
+	~Board();
+
+	bool valid() const;
+	int width() const;
+	int height() const;
+	int winCondition() const;
+
+	std::unique_ptr<Tile> const& tile(Coordinates const& at) const;
+	bool play(IPlayer const& player, Coordinates const& at);
+	bool filled() const;
+	bool empty() const;
+	
+private:
+	struct Impl;
+	std::unique_ptr<Impl> mImpl;
+};
+
+} // namespace ttt
