911fa321bc6fdc26253d746bae58149ab7c618f7
Exequiel Herrera De Rosa <exequiel.herrera@globant.com>
2022-10-31 12:57:46 -0300

    Merge conflicts

--- app/source/PlayerListing.cpp
+++ app/source/PlayerListing.cpp
@@ -6,13 +6,12 @@
 namespace ttt
 {
 
-void setupPlayers(std::unique_ptr<Competition>& forCompetition)
-{
-	forCompetition->addPlayer(std::make_unique<ttt::PlayerExe>("[BOT] EXE_A"));
-	//forCompetition->addPlayer(std::make_unique<ttt::PlayerExample>("[BOT] RANDOM_A"));
-	forCompetition->addPlayer(std::make_unique<ttt::PlayerExe>("[BOT] EXE_B"));
-	//forCompetition->addPlayer(std::make_unique<ttt::PlayerExample>("[BOT] RANDOM_B"));
-	// Add new players below:
-}
+	void setupPlayers(std::unique_ptr<Competition> &forCompetition)
+	{
+		forCompetition->addPlayer(std::make_unique<ttt::PlayerExample>("[BOT] Ludwig Van Example"));
+
+		// Add new players below:
+		forCompetition->addPlayer(std::make_unique<ttt::PlayerExe>("[BOT] ExeA"));
+	}
 
 } // namespace
